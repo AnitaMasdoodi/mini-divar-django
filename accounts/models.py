fcf8a3aba179cce906cf98d04d267b5efb30034f
AnitaMasdoodi <anitamasdoodi.2000@gmail.com>
2025-10-27 20:17:16 +0330

    Add custom User model and initial migration

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class User(AbstractUser):
+    phone_number = models.CharField(max_length=11, unique=True)
+    first_name = models.CharField(max_length=128)
+    last_name = models.CharField(max_length=128)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
